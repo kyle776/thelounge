8538b6591657a2b003bd19457c8468c2ebf8997d
Kyle Jobbins <kyle@jobbins.net>
2025-10-19 11:34:47 +0100

    feat: Update header.html styling with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 module.exports = {
   content: [
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/*.md',
-    './*.html',
-    './*.md',
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.md",
+    "./*.html",
+    "./*.md",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "lounge-blue": "rgb(50, 65, 94)",
+        "lounge-red": "rgb(201, 101, 112)",
+      },
+    },
   },
   plugins: [],
-}
+};
